8974984f86374e086a1a9480dcb5d83ff010afeb
schreyerpeter <pschreyer12321@aol.com>
2016-10-18 21:16:57 -0700

    Initialized state and input is now set equal to property in state object.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 //This way we do not have to say extends React.Component
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { term : '' };
+  }
   render() {
     return(
-       <input onChange={event => console.log(event.target.value)}/>
-    )
+       <div>
+        <input onChange={event => this.setState({ term: event.target.value })}/>
+       </div>
+    );
   }
 }
 
