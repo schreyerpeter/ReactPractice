acdc89d0f3366845a140329c991a9de77fb24718
schreyerpeter <pschreyer12321@aol.com>
2016-10-19 21:04:56 -0700

    App is not class based and includes callback function to set YouTube API call to state.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBoa9AEyuYfLbNDV_RMYxL7AsdGjohcZ0w';
 
 //Create a new component. This component will produce HTML.
-const App = () => {
-  return (
-    <div>
+//The most parent component should be in charge of fetching data
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key:API_KEY, term:'surfboards'}, videos => {
+      this.setState({ videos });
+      //this.setState({videos:videos}); only works when key and property have same name
+    })
+  }
+
+  render() {
+    return (
+      <div>
       <SearchBar />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 //Append the component's generated HTML to the DOM
