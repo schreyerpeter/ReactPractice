004c8da68da3647a600d60f7d93df819bcffc394
schreyerpeter <pschreyer12321@aol.com>
2016-10-26 21:45:21 -0700

    App has limited functionality.

---
+++ src/components/video_list.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video} />
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList;
+// class VideoList extends Component{
+//   constructor(props){
+//     super(props);
+//
+//     this.state = {}
+//   }
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBoa9AEyuYfLbNDV_RMYxL7AsdGjohcZ0w';
 
@@ -12,11 +14,17 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key:API_KEY, term:'surfboards'}, videos => {
-      this.setState({ videos });
-      //this.setState({videos:videos}); only works when key and property have same name
+    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
+      //could pass this.setState({videos}); only works when key and property have same name
     })
   }
 
@@ -24,6 +32,10 @@ class App extends Component {
     return (
       <div>
       <SearchBar />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
