80a370ba4e4d88a434147abf739b7b28f1091dcd
schreyerpeter <pschreyer12321@aol.com>
2016-11-01 21:02:50 -0700

    Search functionality is now in place and suggested videos update.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -19,7 +20,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term:term}, (videos) => {
       this.setState({
         videos:videos,
         selectedVideo: videos[0]
@@ -29,9 +34,13 @@ class App extends Component {
   }
 
   render() {
+    //debounce takes inner function and returns a function that can only
+    //be called once every 300 ms in this case
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={videoSearch}/>
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
